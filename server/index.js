ac89b20b716b30c6f5169f642ea3f8eede73544e
Tufayl <tufaylasaf@gmail.com>
2023-11-30 23:48:59 +0530

    dsa

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const User = require("../models/user");
 const { mongoose } = require("mongoose");
 const cookieParser = require("cookie-parser");
 import cors from "cors";
