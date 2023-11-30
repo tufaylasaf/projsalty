796cf84677f9a5dcd281897189e169e1398dcb47
Tufayl <tufaylasaf@gmail.com>
2023-11-30 22:30:00 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -6,17 +6,6 @@ import cors from "cors";
 
 const app = express();
 
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("Database Connected"))
-  .catch((err) => console.log("Database not Connected", err));
-
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
-
-app.use("/", require("./routes/authRoutes"));
-
 app.use(cors());
 // Allow specific origin(s)
 app.use(
@@ -27,5 +16,23 @@ app.use(
   })
 );
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+
+// app.use("/", require("./routes/authRoutes"));
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Hello There!");
+});
+
 // const port = 8000;
 app.listen(5000, () => console.log(`Server is running on port ${port}`));
+
+mongoose
+  .connect(
+    "mongodb+srv://tufayl:no7158lyafut@cluster0.klpi1zw.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log("Database not Connected", err));
