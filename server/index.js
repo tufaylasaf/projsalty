7abe6f71f653b5b84d12a3a176abe201fcc1511e
Tufayl <tufaylasaf@gmail.com>
2023-11-30 23:10:48 +0530

    dasdas

--- server/index.js
+++ server/index.js
@@ -1,16 +1,14 @@
-import authRoutes from "./routes/authRoutes.js";
-import mongoose from "mongoose";
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { mongoose } = require("mongoose");
+const cookieParser = require("cookie-parser");
 import cors from "cors";
-import express from "express";
-import cookieParser from "cookie-parser";
-
-// const cookieParser = require("cookie-parser");
 
 const app = express();
 
 app.use(express.json());
-// app.use(cookieParser());
-// app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(cors());
 // Allow specific origin(s)
@@ -27,25 +25,14 @@ app.get("/", (request, response) => {
   return response.status(234).send("Hello There!");
 });
 
-// app.use("/", authRoutes);
+// app.use("/", require("./routes/authRoutes"));
 
-app.listen(5000, () => {
-  console.log(`App is listening to port : ${PORT}`);
-});
+// const port = 8000;
+app.listen(5000, () => console.log(`Server is running on port ${port}`));
+
 mongoose
   .connect(
     "mongodb+srv://tufayl:no7158lyafut@cluster0.klpi1zw.mongodb.net/?retryWrites=true&w=majority"
   )
-  .then(() => {
-    console.log("Connected to database");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
-// mongoose
-//   .connect(
-//     "mongodb+srv://tufayl:no7158lyafut@cluster0.klpi1zw.mongodb.net/?retryWrites=true&w=majority"
-//   )
-//   .then(() => console.log("Database Connected"))
-//   .catch((err) => console.log("Database not Connected", err));
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log("Database not Connected", err));
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,24 +1,26 @@
-import express from "express";
+const express = require("express");
 const router = express.Router();
-
+const cors = require("cors");
 const {
+  test,
   registerUser,
   loginUser,
   getProfile,
 } = require("../controllers/authController");
 
 //middleware
-// router.use(
-//   cors({
-//     credentials: true,
-//     origin: "https://projsalty.vercel.app",
-//   })
-// );
+router.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
 
+router.get("/", test);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/profile", getProfile);
 
-export default router;
+module.exports = router;
 
 // "start": "nodemon index.js",
