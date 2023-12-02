917582020639df7e2adf6e93608c911c197d9e8c
Tufayl <tufaylasaf@gmail.com>
2023-12-02 16:04:00 +0530

    auth

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import User from "../models/user.js";
-import { hashPassword, comparePassword } from "../helpers/auth.js";
+import auth from "../helpers/auth.js";
 import jwt from "jsonwebtoken";
 
 // import {
